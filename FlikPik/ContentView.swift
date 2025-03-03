cf63b93c5bb3350d296b4aeea1e3177feb06d1c0
Kunwar Sahni <kunwar.sahni01@gmail.com>
2025-03-03 12:55:38 -0500

    Initial Commit

---
+++ FlikPik/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlikPik
+//
+//  Created by Kunwar Sahni on 3/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlikPik/FlikPikApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlikPikApp.swift
+//  FlikPik
+//
+//  Created by Kunwar Sahni on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FlikPikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
